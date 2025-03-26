12a59b56a839f65e006b7b14234fa17c193b5158
Priyanshu Dahiya <dahiyapriyanshu215@gmail.com>
2025-03-26 19:50:09 +0530

    Add files via upload

---
+++ Arithmetic Operators.py
@@ -0,0 +1,26 @@
+a = 10
+b = 2
+
+# Sum of two numbers
+sum = a + b
+print(sum)
+
+# Difference of two numbers
+diff = a - b
+print(diff)
+
+# Multiplication of two numbers
+mul = a * b
+print(mul)
+
+# Division of two numbers
+div = a / b
+print(div)
+
+# Modulus of two numbers
+mod = a % b
+print(mod)
+
+# Power of two numbers
+pow = a ** b
+print(pow)
