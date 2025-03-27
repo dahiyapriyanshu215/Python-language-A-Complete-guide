ab217e9caaf91c55679e00f25bb5dcdca0398255
Priyanshu Dahiya <dahiyapriyanshu215@gmail.com>
2025-03-27 20:49:36 +0530

    Add files via upload

---
+++ Logical Operators.py
@@ -0,0 +1,16 @@
+a = True
+b = False
+
+# logical not operator
+print (not a)
+
+# logical and operator
+print (a and b)
+
+# logical or operator
+print (a or b)
+
+#important case
+x = 1
+y = 2
+print((a==b) or (a<b))
