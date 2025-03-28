d967469d125311013dcb3a7fffed25c635c424b6
Priyanshu Dahiya <dahiyapriyanshu215@gmail.com>
2025-03-28 18:28:44 +0530

    Add files via upload

---
+++ Relational Operators.py
@@ -0,0 +1,20 @@
+a = 1
+b = 2
+
+# equals to
+print(a == b)
+
+# not equals to
+print(a != b)
+
+# greater than
+print(a > b)
+
+# less than
+print(a < b)
+
+# greater than or equals to
+print(a >= b)
+
+# less than or equals to
+print(a <= b)
