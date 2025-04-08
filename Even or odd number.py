f4a58c743eaa53071c01fec34d62729457a0a9a2
Priyanshu Dahiya <dahiyapriyanshu215@gmail.com>
2025-04-08 16:31:04 +0530

    Add files via upload

---
+++ Even or odd number.py
@@ -0,0 +1,8 @@
+num = int(input("Enter the number:"))
+if(num<=0):
+    print("Wrong choice")
+else:        
+    if(num%2==0):
+        print("The number is even")
+    else:
+        print("The number is odd")
