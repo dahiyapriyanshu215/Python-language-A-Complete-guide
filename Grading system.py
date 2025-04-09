9470b3d6b2d417400e2c9a221297f6389685f99d
Priyanshu Dahiya <dahiyapriyanshu215@gmail.com>
2025-04-09 16:12:01 +0530

    Add files via upload

---
+++ Grading system.py
@@ -0,0 +1,9 @@
+marks = int(input("Enter your total marks out of 100:"))
+if(marks>=90):
+    print("You got A grade")
+elif(marks>=80 and marks<=90):
+    print("You got B grade")
+elif(marks>=70 and marks<=80):
+    print("You got C grade")
+else:
+    print("You got D grade")
