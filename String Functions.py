7c4e474e6809656adc132788934e066fc6f7cc92
Priyanshu Dahiya <dahiyapriyanshu215@gmail.com>
2025-04-17 22:31:39 +0530

    Add files via upload

---
+++ String Functions.py
@@ -0,0 +1,20 @@
+str = "i am a coder"
+
+# endswith()
+print(str.endswith("der"))
+print(str.endswith("age"))
+
+# capitalize()
+print(str.capitalize())
+
+# replace()
+print(str.replace("a","p"))
+
+# find()
+print(str.find("o"))
+print(str.find("a"))
+print(str.find("am"))
+
+# count()
+print(str.count("o"))
+print(str.count("a"))
