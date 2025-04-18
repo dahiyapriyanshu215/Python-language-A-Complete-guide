b1ba71b1e854deecbb5e67bc84f8e9e571384320
Priyanshu Dahiya <dahiyapriyanshu215@gmail.com>
2025-04-18 22:04:32 +0530

    Add files via upload

---
+++ String Operations.py
@@ -0,0 +1,33 @@
+# Concatention
+str1 = "apna"
+str2 = "college"
+print(str1 + str2)
+
+# Length
+ch = len(str1)
+print(ch)
+
+final_string = str1 + str2
+print(final_string)
+
+final_str = str1 +" "+str2
+print(final_str)
+print(len(final_str))
+
+# Indexing
+str = "apna college"
+print(str[1])
+print(str[0] + str[3])
+
+# Slicing
+str = "apna college"
+print(str[1:5])
+print(str[5:12])
+print(str[5:len(str)])
+print(str[5:])
+print(str[:4])
+
+# Negative Indexing
+str = "apple"
+print(str[-3:-1])
+print(str[-5:-2])
