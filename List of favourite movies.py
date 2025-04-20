415ebd980b253505ad68786c644e6b1c1066f7a3
Priyanshu Dahiya <dahiyapriyanshu215@gmail.com>
2025-04-20 20:47:54 +0530

    Add files via upload

---
+++ List of favourite movies.py
@@ -0,0 +1,8 @@
+list1 = []
+a = str(input("Enter the 1st movie:",))
+b = str(input("Enter the 2nd movie:",))
+c = str(input("Enter the 3rd movie:",))
+list1.append(a)
+list1.append(b)
+list1.append(c)
+print(list1)
