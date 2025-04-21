df74bc62e955a7976a0e5e5a246fb56b1f599916
Priyanshu Dahiya <dahiyapriyanshu215@gmail.com>
2025-04-21 21:55:09 +0530

    Add files via upload

---
+++ Palindrome list or not.py
@@ -0,0 +1,16 @@
+list1 = [1,2,3,2,1]
+list2 = list1.copy()
+rev_list1 = list1.reverse()
+if(list1 == list2):
+    print("Palindrome")
+else:
+    print("Not Palindrome")
+
+
+list3 = [1,"abc","abc",1]
+list4 = list3.copy()
+rev_list3 = list3.reverse()
+if(list3 == list4):
+    print("Palindrome")
+else:
+    print("Not Palindrome")
