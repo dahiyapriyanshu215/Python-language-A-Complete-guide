e5ae3240a0d55b9c2b7fbfd0176af5000f7b821c
Priyanshu Dahiya <dahiyapriyanshu215@gmail.com>
2025-05-02 08:10:11 +0530

    Add files via upload

---
+++ classrooms sets for various subjects.py
@@ -0,0 +1,4 @@
+subjects = {"python","java","C++","python","javascript","java","python","java","C++","C"}
+print("given unique subjects are",subjects)
+print("No. of classrooms needed are",len(subjects))
+print(type(subjects))
